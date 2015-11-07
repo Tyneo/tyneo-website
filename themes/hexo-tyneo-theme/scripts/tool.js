3f0416b099a2e3cc612667ea386c79f54ab6d9d8
Samuel Kauffmann <skauffmann@tyneo.net>
2015-11-07 15:42:48 +0000

    update

---
+++ themes/hexo-tyneo-theme/scripts/tool.js
@@ -0,0 +1,17 @@
+/* global hexo */
+/**
+ *  This script will generate all training page from training data
+ */
+ 
+hexo.extend.generator.register('tool', function (locals) {
+    var routes = [];
+    var data = hexo.locals.get('data');
+    if(data && data.tools) {
+        for(var i=0; i<data.tools.tools.length; i++) {
+            var tool = data.tools.tools[i];
+            var content = hexo.render.renderSync({path: 'themes/hexo-tyneo-theme/layout/tool.ejs'}, {tool: tool, site: locals});
+            routes.push({path:tool.file, data:{content: content}, layout: 'simple'});
+        }
+    }
+    return routes;
+});
