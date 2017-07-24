8cd41df5f3f4f138615aabb6254a1d6f5a6da133
Samuel Kauffmann <skauffmann@tyneo.net>
2017-07-24 22:42:05 +0200

    full erase

--- themes/hexo-tyneo-theme/scripts/i18n.js
+++
@@ -1,8 +0,0 @@
-/* global hexo */
-
-hexo.extend.helper.register('i18n_url', function(url){
-    var lang = this.page.lang;
-    if(!lang || lang == '') lang = 'fr';
-    var i18n_url = url.replace('[lang]', lang);
-    return this.url_for(i18n_url);
-});
