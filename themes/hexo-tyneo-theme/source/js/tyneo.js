0bec1fc8f19fb16dccdcc336b5340e84411e22f1
skauffmann <skauffmann@tyneo.net>
2018-12-13 12:01:11 +0100

    Change js event from blur to change

--- themes/hexo-tyneo-theme/source/js/tyneo.js
+++ themes/hexo-tyneo-theme/source/js/tyneo.js
@@ -1,6 +1,6 @@
 $(function() {
   //Only display newsletter captcha when start editing email adress field
-  $('#email-field-7341').blur(function() {
+  $('#email-field-7341').change(function() {
     $('#newsletter-captcha-7341').show();
   });
   $('a[href*=#]:not([href=#])').click(function() {
